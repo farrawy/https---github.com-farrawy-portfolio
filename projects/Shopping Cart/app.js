3945af055996e25c2640f2896c1eaeb4e4b4d6cc
Farrawy <ahmed.e.alfarra@gmail.com>
2022-03-11 04:23:51 +0300

    shopping website added

---
+++ projects/Shopping Cart/app.js
@@ -0,0 +1,36 @@
+// let menu = document.querySelector('#menu-bar');
+// let navbar = document.querySelector('.navbar');
+// let shoppingCart = document.querySelector('#fa-shopping-cart');
+
+
+
+
+// menu.addEventListener('click', () => {
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+// });
+
+// window.onscroll = () => {
+//     menu.classList.remove('fa-times');
+//     navbar.classList.remove('active');
+// }
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
+
+// (function(){
+
+//     $("#cart").on("click", function() {
+//         $(".shopping-cart").fadeToggle( "fast");
+//     });
+
+// })();
+
